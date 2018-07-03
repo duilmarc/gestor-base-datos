63780e1205f19fa1091797e4dd691d3cbe9c3cda
Franco Duilmarc Chavez Ponce <franco.chp@gmail.com>
2018-07-02 23:03:19 -0500

    Update indexing_sort.cpp

--- indexing_sort.cpp
+++ indexing_sort.cpp
@@ -1,104 +1,208 @@
+
 #include <fstream>
 #include <stdio.h>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>    // std::sort
-#include "estructuras.h"
+#include <algorithm>
 
 using namespace std;
 
-/*
-int particion(vector<pair<string,int>> A,int p,int r){
-    int pivot=stoi(A[r].first);
-    int i=(p-1);
-    for(int j=p;j<=r-1;j++){
-        if(stoi(A[j].first)<=pivot){
-            i++;
-            swap(A[i].first,A[j].first);
-            swap(A[i].second,A[j].second);
-        }
-    }
-    swap(A[i+1].first,A[r].first);
-    swap(A[i+1].second,A[r].second);
-    return (i+1);
-}
-void quicksort(vector<pair<string,int>> &A,int p,int r){
-    int q;
-    if(p<r){
-        q=particion(A,p,r);
-        quicksort(A,p,q-1);
-        quicksort(A,q+1,r);
-    }
-}
-*/
-/*void quicksort(vector<pair<string,int>> str,int p ,int r){
-    int i=p,j=r;
-    int mid=str[(p+r)/2].first;
-    while(i<=j){
-        while(str[i].first<mid)
-            i++;
-        while(str[j].first>mid)
-            j--;
-        if(i<=j){
-            swap(str[i].first,str[j].first);
-            swap(str[i].second,str[j].second);
-            i++;j--;
-        }
-    };
-    if(p<j){
-        quicksort(str,p,j);
-    }
-    if(i<r){
-        quicksort(str,i,r);
-    }
-}
-*/
 
-
-void indexar_tabla(string nombre_archivo,int numero_columna)
+bool indexar_tabla(string archivo_entrada,int numero_columna,string archivo_salida)
 {
-    ifstream is (nombre_archivo, std::ifstream::binary);
-    ofstream ind("machine_attributes/part-00000-of-00001_ordenado.txt",ios::out | ios::binary);
+    ifstream is (archivo_entrada, std::ifstream::binary);
+    ofstream ind(archivo_salida,std::ofstream::app);
       if (is) {
-        //long int i=0;
-        long int j=0;
         char * buffer = new char [200];
+        string convert;
+        vector<pair<long int,long int>> index;
         long int posicion;
-        vector<pair<string,int>> index;
         while(!is.eof())
         {
             for(int i=1;i<=numero_columna;i++){
                 if(i==1)
-                    posicion=is.tellg();
-
-                is.getline(buffer,200,',');
-                if(i==numero_columna)
                 {
-                    index.push_back(make_pair(buffer,posicion));                    
+                    posicion=is.tellg();
+                }
+                is.getline(buffer,50,',');
+                if(i==numero_columna)
+                {   
+                    index.push_back(make_pair(atol(buffer),posicion));
                     is.getline(buffer,200);
-                    j++;
                 }
             }
         }
         sort(index.begin(),index.end());
-        //radix_sort(index);
         for(unsigned long long int i=0;i<index.size()-1;i++){
             ind<<index[i].first<<","<<index[i].second<<endl;
-            
         }
-        
         delete[] buffer;
         ind.close();
         is.close();
+      }
+    else
+    {
+        return false;    
+    }
+    return true;
+}
+
+void indexar_machine_atributes(int numero_columna)
+{
+    string entrada="/home/duilmarc/Documents/bd2/proyecto_Base_Datos/clusterdata-2011-2/machine_attributes/part-00000-of-00001.csv";
+    string salida="/home/duilmarc/Documents/bd2/proyecto_Base_Datos/clusterdata-2011-2/machine_attributes/indice.txt";
+    if(indexar_tabla(entrada,numero_columna,salida))
+        cout<<endl<<"indexacion exitosa";
+    else
+        cout<<endl<<"error durante la indexacion";
+}
+
+void indexar_machine_events(int numero_columna)
+{
+    string entrada="/home/duilmarc/Documents/bd2/proyecto_Base_Datos/clusterdata-2011-2/machine_events/part-00000-of-00001.csv";
+    string salida="/home/duilmarc/Documents/bd2/proyecto_Base_Datos/clusterdata-2011-2/machine_events/indice2.txt";
+    if(indexar_tabla(entrada,numero_columna,salida))
+        cout<<endl<<"indexacion exitosa";
+    else
+        cout<<endl<<"error durante la indexacion";
+}
+
+void indexar_task_events(int numero_columna)
+{
+    string parte1="/home/duilmarc/Documents/bd2/proyecto_Base_Datos/clusterdata-2011-2/task_events/part-00";
+    string parte3="-of-00500.csv";
+    string parte2="";
+    string entrada;
+    string salida="/home/duilmarc/Documents/bd2/proyecto_Base_Datos/clusterdata-2011-2/task_events/indice.txt";
+    for(int i=0;i<500;i++)
+    {
+        if(i<10)
+        {
+           parte2="00"+to_string(i);
+        }
+        else if(i<100)
+        {
+           parte2="0"+to_string(i);
+        }
+        else
+        {
+            parte2=to_string(i);
+        }
+        ofstream ind(salida,std::ofstream::app);
+        cout<<"indexando parte "+parte2<<endl;
+        ind<<"archivo"<<"-"<<parte2<<endl;
+        ind.close();
+        entrada=parte1+parte2+parte3;
+        if(indexar_tabla(entrada,numero_columna,salida))
+            cout<<endl<<"indexacion exitosa"<<endl;
+        else
+            cout<<endl<<"error durante la indexacion"<<endl;
+
     }
 }
 
+void indexar_task_constraints(int numero_columna)
+{
+    string parte1="/home/duilmarc/Documents/bd2/proyecto_Base_Datos/clusterdata-2011-2/task_constraints/part-00";
+    string parte3="-of-00500.csv";
+    string parte2="";
+    string entrada;
+    string salida="/home/duilmarc/Documents/bd2/proyecto_Base_Datos/clusterdata-2011-2/task_constraints/indice.txt";
+    for(int i=0;i<500;i++)
+    {
+        if(i<10)
+        {
+           parte2="00"+to_string(i);
+        }
+        else if(i<100)
+        {
+           parte2="0"+to_string(i);
+        }
+        else
+        {
+            parte2=to_string(i);
+        }
+        ofstream ind(salida,std::ofstream::app);
+        cout<<"indexando parte "+parte2<<endl;
+        ind<<"archivo"<<"-"<<parte2<<endl;
+        ind.close();
+        entrada=parte1+parte2+parte3;
+        if(indexar_tabla(entrada,numero_columna,salida))
+            cout<<endl<<"indexacion exitosa"<<endl;
+        else
+            cout<<endl<<"error durante la indexacion"<<endl;
+    }
+}
 
+void indexar_task_usage(int numero_columna)
+{
+    string parte1="/home/duilmarc/Documents/bd2/proyecto_Base_Datos/clusterdata-2011-2/task_usage/part-00";
+    string parte3="-of-00500.csv";
+    string parte2="";
+    string entrada;
+    string salida="/home/duilmarc/Documents/bd2/proyecto_Base_Datos/clusterdata-2011-2/task_usage/indice.txt";
+    for(int i=0;i<500;i++)
+    {
+        if(i<10)
+        {
+           parte2="00"+to_string(i);
+        }
+        else if(i<100)
+        {
+           parte2="0"+to_string(i);
+        }
+        else
+        {
+            parte2=to_string(i);
+        }
+        ofstream ind(salida,std::ofstream::app);
+        cout<<"indexando parte "+parte2<<endl;
+        ind<<"archivo"<<"-"<<parte2<<endl;
+        ind.close();
+        entrada=parte1+parte2+parte3;
+        if(indexar_tabla(entrada,numero_columna,salida))
+            cout<<endl<<"indexacion exitosa"<<endl;
+        else
+            cout<<endl<<"error durante la indexacion"<<endl;
+    }
+}
+void indexar_job_events(int numero_columna)
+{
+    string parte1="/home/duilmarc/Documents/bd2/proyecto_Base_Datos/clusterdata-2011-2/job_events/part-00";
+    string parte3="-of-00500.csv";
+    string parte2="";
+    string entrada;
+    string salida="/home/duilmarc/Documents/bd2/proyecto_Base_Datos/clusterdata-2011-2/job_events/indice.txt";
+    for(int i=0;i<500;i++)
+    {
+        if(i<10)
+        {
+           parte2="00"+to_string(i);
+        }
+        else if(i<100)
+        {
+           parte2="0"+to_string(i);
+        }
+        else
+        {
+            parte2=to_string(i);
+        }
+        ofstream ind(salida,std::ofstream::app);
+        cout<<"indexando parte "+parte2<<endl;
+        ind<<"archivo"<<"-"<<parte2<<endl;
+        ind.close();
+        entrada=parte1+parte2+parte3;
+        if(indexar_tabla(entrada,numero_columna,salida))
+            cout<<endl<<"indexacion exitosa"<<endl;
+        else
+            cout<<endl<<"error durante la indexacion"<<endl;
+    }
+}
 int main(int argc, char *argv[])
 {
 
-    string nombre="machine_attributes/part-00000-of-00001.csv";
-    indexar_tabla(nombre,2);
-
+    indexar_machine_atributes(2);
+    //indexar_job_events(2);
+    return 0;
 }
